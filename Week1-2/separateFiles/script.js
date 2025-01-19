a7833ebb2b629590aac86996f006b97fbec345ad
Jussipekkak <jussi.karhumaa@centria.fi>
2025-01-19 14:04:06 +0200

    Add final content

---
+++ Week1-2/separateFiles/script.js
@@ -0,0 +1,15 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    console.log('Nimi:', name);
+    console.log('Sähköposti:', email);
+    console.log('Viesti:', message);
+
+    alert('Lomake lähetetty onnistuneesti!');
+
+    document.getElementById('contactForm').reset();
+});
